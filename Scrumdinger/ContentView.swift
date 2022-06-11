856a7306c39c039f6b9ac2913f72a515b6704678
Jaykumar <jaykumar.hiranandani@gmail.com>
2022-06-11 11:07:19 -0400

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Jaykumar on 2022-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
