eea49d9909ef01527b2c8329e97fe01467c240cd
Jaykumar <jaykumar.hiranandani@gmail.com>
2022-06-11 12:02:52 -0400

    made the Meeting View - version 1

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -10,11 +10,31 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ProgressView(value: 7.5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds remaining")
+                        .font(.caption)
+                    Label("750", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
 
