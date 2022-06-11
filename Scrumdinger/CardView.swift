873570b919a1bebb96bdfc34b05747f7c7766022
Jaykumar <jaykumar.hiranandani@gmail.com>
2022-06-11 13:49:25 -0400

    added cardview with basic elements; added TrailingIconLabelStyle

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,39 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Jaykumar on 2022-06-11.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .font(.caption)
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .font(.caption)
+                    .labelStyle(.trailingIcon)
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.sampleData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
