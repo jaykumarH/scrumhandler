ef1f33b92a8f237e645fc83ef29145967946b44f
Jaykumar <jaykumar.hiranandani@gmail.com>
2022-07-15 19:09:34 -0400

    change for avplayer

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -6,40 +6,54 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var timer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
     var body: some View {
         ZStack {
-            MeetingHeaderView(
-                secondsElapsed: 60,
-                secondsRemaining: 180,
-                theme: scrum.theme
-            )
             RoundedRectangle(cornerRadius: 16.0)
                 .fill(scrum.theme.mainColor)
             VStack {
+                MeetingHeaderView(
+                    secondsElapsed: timer.secondsElapsed,
+                    secondsRemaining: timer.secondsRemaining,
+                    theme: scrum.theme
+                )
                 Circle()
                     .strokeBorder(lineWidth: 24)
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel("Next speaker")
-                }
+                MeetingFooterView(
+                    speakers: timer.speakers,
+                    skipAction: timer.skipSpeaker
+                )
             }
         }
         .padding()
         .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            timer.reset(
+                lengthInMinutes: scrum.lengthInMinutes,
+                attendees: scrum.attendees
+            )
+            timer.startScrum()
+            timer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
+        }
+        .onDisappear {
+            timer.stopScrum()
+        }
         .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+        MeetingView(scrum: .constant(DailyScrum.sampleData[1]))
     }
 }
